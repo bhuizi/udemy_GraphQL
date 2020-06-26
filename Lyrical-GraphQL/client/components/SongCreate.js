3c66633aafafe3a84ba1528724761f79fcf126d9
Bryan Huizi <bryan.huizi@magnet360.com>
2020-06-26 16:46:55 -0700

    ClientSide

--- Lyrical-GraphQL/client/components/SongCreate.js
+++ Lyrical-GraphQL/client/components/SongCreate.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-function SongCreate() {
-    return <div>create a song</div>
+class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "" };
+  }
+  onSubmit(event){
+    event.preventDefault();
+    this.props.mutate({
+        variables: {
+            title: this.state.title
+        }
+    })
+    this.setState({title: ''});
+  };
+  render() {
+    return (
+      <div>
+        <h4>create a song</h4>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <label>Song Title</label>
+          <input
+            value={this.state.title}
+            onChange={(event) =>
+              this.setState({ title: event.currentTarget.value })
+            }
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default SongCreate;
+const mutation = gql`
+  mutation AddSong($title: String){
+    addSong(title: $title ) {
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(SongCreate);
--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,16 +1,22 @@
 import React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+
 function SongList({ data }) {
   const { loading, songs } = data;
   function renderSongs() {
-    return songs.map((song) => 
-    <li key={song.id} className="collection-item">{song.title}</li>);
+    return songs.map((song) => (
+      <li key={song.id} className="collection-item">
+        {song.title}
+      </li>
+    ));
   }
   return (
     <div>
-        <h4>Song List</h4>
-      <ul className="collection">{loading ? <li>loading...</li> : renderSongs()}</ul>
+      <h4>Song List</h4>
+      <ul className="collection">
+        {loading ? <li>loading...</li> : renderSongs()}
+      </ul>
     </div>
   );
 }
@@ -18,7 +24,7 @@ function SongList({ data }) {
 const query = gql`
   {
     songs {
-      title,
+      title
       id
     }
   }
